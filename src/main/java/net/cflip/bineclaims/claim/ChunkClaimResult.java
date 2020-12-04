c6787088cbcb1472ca91866da1759b8c0053174c
Cflip <36554078+cflip@users.noreply.github.com>
2020-12-04 16:44:23 -0700

    Add colours to command messages

--- src/main/java/net/cflip/bineclaims/claim/ChunkClaimResult.java
+++ src/main/java/net/cflip/bineclaims/claim/ChunkClaimResult.java
@@ -2,10 +2,11 @@ package net.cflip.bineclaims.claim;
 
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 
 public enum ChunkClaimResult {
-	ALREADY_CLAIMED(new TranslatableText("command.claim.already_claimed")),
-	SUCCESS(new TranslatableText("command.claim.success"));
+	ALREADY_CLAIMED(new TranslatableText("command.claim.already_claimed").formatted(Formatting.RED)),
+	SUCCESS(new TranslatableText("command.claim.success").formatted(Formatting.GREEN));
 
 	public Text message;
 
