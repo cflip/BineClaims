818b19487e1d0bcd47727c97036c799e19dbe4ee
Cflip <36554078+cflip@users.noreply.github.com>
2020-12-06 21:27:44 -0700

    Add callback to prevent block placement in claimed chunks

--- src/main/java/net/cflip/bineclaims/BineClaims.java
+++ src/main/java/net/cflip/bineclaims/BineClaims.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 
@@ -19,6 +20,19 @@ public class BineClaims implements ModInitializer {
 		});
 
 		AttackBlockCallback.EVENT.register((playerEntity, world, hand, blockPos, direction) -> {
+			if (!world.isClient) {
+				ServerPlayerEntity serverPlayer = (ServerPlayerEntity) playerEntity;
+
+				if (guildManager.canInteract(serverPlayer)) {
+					return ActionResult.PASS;
+				} else {
+					return ActionResult.FAIL;
+				}
+			}
+			return ActionResult.PASS;
+		});
+
+		UseBlockCallback.EVENT.register((playerEntity, world, hand, blockPos) -> {
 			if (!world.isClient) {
 				ServerPlayerEntity serverPlayer = (ServerPlayerEntity) playerEntity;
 
