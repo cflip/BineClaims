73ff056ff87e9ada378270d3e661615470516892
Cflip <36554078+cflip@users.noreply.github.com>
2020-12-10 17:01:38 -0700

    Add command to list all guilds

--- src/main/java/net/cflip/bineclaims/command/BineClaimsCommand.java
+++ src/main/java/net/cflip/bineclaims/command/BineClaimsCommand.java
@@ -4,17 +4,22 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.cflip.bineclaims.BineClaims;
 import net.cflip.bineclaims.guild.Guild;
 import net.cflip.bineclaims.guild.GuildInterface;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
 
 public class BineClaimsCommand {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(CommandManager.literal("bclaims")
 			.then(CommandManager.literal("claim").executes(BineClaimsCommand::claim))
 			.then(CommandManager.literal("owner").executes(BineClaimsCommand::owner))
+			.then(CommandManager.literal("list").executes(BineClaimsCommand::listGuilds))
 			.then(CommandManager.literal("guild")
 				.then(CommandManager.literal("create").then(CommandManager.argument("name", StringArgumentType.greedyString()).executes(BineClaimsCommand::guildCreate)))
 				.then(CommandManager.literal("join").then(CommandManager.argument("guild", new GuildArgumentType()).executes(BineClaimsCommand::guildJoin)))
@@ -37,6 +42,15 @@ public class BineClaimsCommand {
 		BineClaimsCommandResult result = GuildInterface.getOwner(player.chunkX, player.chunkZ, player.getServerWorld().getRegistryKey());
 		context.getSource().sendFeedback(result.getMessage(), false);
 		return result.type;
+	}
+
+	public static int listGuilds(CommandContext<ServerCommandSource> context) {
+		ServerCommandSource source = context.getSource();
+		if (!source.getWorld().isClient) {
+			Text result = Texts.join(BineClaims.guildManager.getGuildNames(), LiteralText::new);
+			source.sendFeedback(result, false);
+		}
+		return 0;
 	}
 
 	public static int guildCreate(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
